1d28b65bc5df2d791fc98ba87a1f95fd15df3be9
zhangzhi <z2care@gmail.com>
2014-01-09 00:41:44 +0800

    add i18n python code part.

--- home.py
+++ home.py
@@ -9,6 +9,7 @@ from google.appengine.ext import ndb
 
 import webapp2
 import jinja2
+import gettext
 
 import os, logging
 
@@ -18,7 +19,7 @@ from model import *
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'templates','default')),
-    extensions=['jinja2.ext.autoescape'],
+    extensions=['jinja2.ext.autoescape','jinja2.ext.i18n'],
     autoescape=True)
 
 #START: RenderPage
@@ -26,12 +27,27 @@ class HomePage(webapp2.RequestHandler):
     def get(self):
         domain=os.environ['HTTP_HOST']
         baseurl="https://"+domain
-        
+
+        lang = self.request.GET.get('locale', 'zh_CN')
+
+#http://docs.python.org/2/library/gettext.html
+#the one way
+#        gettext.install('zh_CN', 'locale', unicode=True)
+#        tr = gettext.translation('messages', 'locale', languages=['zh_CN'])#languages as list forms
+#        tr.install(True)#install _() function,***
+#another way
+        tr=gettext.translation('messages','locale',fallback=True,languages=['zh_CN'],codeset='utf-8')
+        tr.install(unicode=True, names=['gettext', 'ngettext'])
+
+#the same part
+        JINJA_ENVIRONMENT.install_gettext_translations(tr)
         template_values = {
             'page_title': 'Home',
             'home_active': 'active',
             'baseurl': baseurl,
         }
+
+        
         template = JINJA_ENVIRONMENT.get_template('home.html')
         self.response.write(template.render(template_values))
         
