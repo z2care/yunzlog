fd9ff537b4a74a0365bdf17cb650da34f3fc3c91
zhangzhi <z2care@gmail.com>
2014-02-13 23:47:03 +0800

    add memcache sample code in model.

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ Created on 2013-12-12
 @author: zhangzhi
 '''
 
+from google.appengine.api import memcache
 from google.appengine.ext import ndb
 import logging
 
@@ -17,3 +18,9 @@ class ConfigSite(ndb.Model):
 class Welcome(ndb.Model):
     words = ndb.StringProperty(required=True)
     is_show = ndb.BooleanProperty(default=True)
+
+    def cache_set(self):
+        memcache.set(self.key().name(), self, namespace=self.key().kind())
+    def put(self):
+        self.cache_set()
+        ndb.Model.put(self)
