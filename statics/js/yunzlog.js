fc739ebdf91a3410402ebfb800b2f6b6de63c09c
zhangzhi <z2care@gmail.com>
2014-07-15 17:00:10 +0800

    content could append,but auth code not refresh.

--- statics/js/yunzlog.js
+++ statics/js/yunzlog.js
@@ -1,24 +1,48 @@
 function ref_code(obj){
     obj.src = obj.src;
 }
-
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+  {
+  c_start=document.cookie.indexOf(c_name + "=")
+  if (c_start!=-1)
+    { 
+    c_start=c_start + c_name.length+1 
+    c_end=document.cookie.indexOf(";",c_start)
+    if (c_end==-1) c_end=document.cookie.length
+    return unescape(document.cookie.substring(c_start,c_end))
+    } 
+  }
+return ""
+}
 function smt_func(){
-    //var authcode = $.cookie("yunzlog_authcode",  { path: '/' });
-    $.ajax({
-      url: "/blog/comment",
-      type: 'POST',
-      data: {
-        cmtext: $("#cmtext").val(),
-        keyid: $("#keyid").val(),
-        name: $("#name").val(),
-        email: $("#email").val()
-      },
-      //get comment block and fill them with response data
-      success: function(response) {
-        $("#cmt_author").html($("#name").val());
-        $("#cmt_ctt").html($("#cmtext").val());
-        $( response ).prependTo($("#cmt_ajax").html());
-      },
-    });
+    var authcode = getCookie("yunzlog_authcode");
+    var inputcode = $("#cmt_code").val();
+    if(authcode.toLowerCase()==inputcode.toLowerCase()){
+      $.ajax({
+        url: "/blog/comment",
+        type: 'POST',
+        data: {
+          cmtext: $("#cmtext").val(),
+          keyid: $("#keyid").val(),
+          name: $("#name").val(),
+          email: $("#email").val()
+        },
+        //get comment block and fill them with response data
+        success: function(response) {
+          $("#cmt_author").html($("#name").val());
+          $("#cmt_ctt").html($("#cmtext").val());
+          //need change date
+          $("#comments-list").prepend($("#cmt_ajax").html());
+          $('#cmt_form')[0].reset();
+        },
+      });
+    }else{
+      alert("auth code error!authcode="+authcode);
+      $('#cmt_code').val("");
+      var obj = $("#smt_img"); 
+      ref_code(obj);
+    }
     return false;
 }
